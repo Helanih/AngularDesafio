b06f54aea410a242682c6ac7e7352dd262bd625c
ANIRAM HERRERA <elanih@gmail.com>
2024-03-20 20:31:12 -0300

    AngularDesafio1

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component'
+@NgModule({
+  declarations: [
+    AppComponent
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class Appmodule { }
